459fd6b9378123c63c1ce40d6ce3cc810c603f88
timlin940 <aa0909095679@gmail.com>
2024-04-18 19:09:25 +0800

    tim_work

---
+++ resources/views/tim_input.blade.php
@@ -0,0 +1,52 @@
+
+@extends('layouts.app')<!--因為有 extends所以不需要再做layout(nmsl TimLin)的部分，另外因為有session(我不懂，記得問人)的關係，這裡的文件不需要用html作為開頭-->
+<!--bootstrap很好用，要學一下-->
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+                    <div class="card-body">
+                        @if(session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+                        {{ Auth::user()->name }}   
+                        @if(Auth::user()->name == 'TimLin')
+                            <br>
+                            奶油好讚
+                        @endif
+                    </div>
+
+                    @if(Auth::user()->name == 'TimLin')
+                    <div>
+                        <form action="{{route('timcreate')}}" method='POST'><!--上傳資料 其他有 Delete update-->
+                                @csrf
+                                <div class="name">
+                                    <p>留言人</p>
+                                        <input type="text" name="inputer" >
+                                </div>
+                                <div class="comment">
+                                    <p>留言區</p>
+                                    <textarea   name='content'
+                                                row='6'
+                                                cols='40'
+                                                placeholder="想說啥"></textarea>
+                                </div>
+                                <div class="submit_comment">
+                                    <input type="submit" value="送出留言" name="submit">
+                                    <br/>
+                                </div>
+                        </form>
+                    </div>
+                    @endif     
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+            <!--{$post}會直接輸出物件-->
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\TimLinController;//要記的cache
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -20,4 +20,13 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-老三;
+
+Route::get('/timindex',[App\Http\Controllers\TimLinController::class,'timindex'])->name('timindex')->middleware('auth');//命名
+
+Route::post('timindex/timcreate',[TimLinController::class,'timcreate'])->name('timcreate');
+
+Route::post('timindex/timdelete/{id}',[TimLinController::class,'timdelete'])->name('timdelete');
+
+Route::post('timindex/timchange/{id}',[TimLinController::class,'timchange'])->name("timchange");
+
+Route::post('timindex/timlastpage',[TimLinController::class,"timlastpage"])->name('timlastpage');// get取得資源 post修改資源 put/patch用來修改資料 delete用來刪除資料 any都處理 match只處理部分的route(array)
